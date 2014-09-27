8601aaa4ab06b6f8cc5d6e1706e856b62f082f7f
Tristan Rice <rice@outerearth.net>
2014-09-27 15:13:26 -0700

    Added reviews

--- client/app/course/course.controller.js
+++ client/app/course/course.controller.js
@@ -3,4 +3,17 @@
 angular.module('ubcCourseApp')
   .controller('CourseCtrl', function ($scope, $stateParams, DataService) {
     $scope.course = DataService.getCourse($stateParams.courseId);
+    $scope.reviewVisible = "none";
+    $scope.expand = function(review) {
+      review.expanded = !review.expanded;
+      console.log($scope.course.reviews, review);
+    };
+    $scope.openReview = function() {
+      $scope.reviewVisible = "block";
+    }
+    $scope.closeReview = function() {
+      $scope.reviewVisible = "none";
+    }
+    $scope.report = function(review) {
+    }
   });
