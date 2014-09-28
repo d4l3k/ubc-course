cf84c58ae6700db7064e6c77028cb49d3841d47a
Tristan Rice <rice@outerearth.net>
2014-09-28 02:38:18 -0700

    Configured mina deploy properly

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,56 @@
-require 'bundler'
-Bundler.require
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
-set :application, "UBCourses"
-set :domain, "raven.fn.lc"
-set :deploy_to, "/srv/http/ubc-courses"
-set :repository, 'https://github.com/d4l3k/ubc-courses.git'
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
 
+set :domain, 'raven.fn.lc'
+set :deploy_to, '/srv/http/ubc-courses'
+set :repository, 'https://github.com/d4l3k/ubc-course.git'
+set :branch, 'master'
 
-task :deploy do
+# Optional settings:
+#   set :user, 'foobar'    # Username in the server to SSH to.
+#   set :port, '30000'     # SSH port number.
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .rbenv-version to your repository.
+  # invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
   deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
     invoke :'git:clone'
-    invoke :'npm:install'
-    invoke :'bundle'
-    invoke :'grunt:install'
-    invoke :'grunt:build'
+    invoke :'bundle:install'
+    queue %[npm install --python=/usr/bin/python2]
+    queue %[bower install]
+    queue %[grunt build]
+
+    to :launch do
+      queue "pm2 restart ubcourses"
+    end
   end
 end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
+
