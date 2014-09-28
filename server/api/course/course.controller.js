a7e90d641c87c2bca90e3b8bea34103778ee9eaf
Matas <Matas@republicproject.com>
2014-09-27 18:40:56 -0700

    Added /api/course end point

---
+++ server/api/course/course.controller.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var _ = require('lodash');
+var pg = require('pg');
+
+  
+// Get list of coursess
+exports.index = function(req, res) {
+	pg.connect("postgres://postgres:@localhost/ubcourses", function(err, client, done)
+	{
+		if(err)
+		{
+			return console.error('A problem with the connection occurred', err);
+		}
+		client.query('SELECT * FROM courses WHERE id=$1', [req.param('course')], function(err, result)
+		{
+			if(err)
+			{
+				return console.error("The query resulted in an error", err);
+			}
+			var course = result.rows[0];
+			client.query('SELECT avg("easy"::integer) AS easy, avg("like"::integer) AS like, avg("useful"::integer) AS useful from reviews WHERE course_id = $1', [req.param('course')], function(err, result){
+			
+				course = _.merge(course, result.rows[0]);
+				client.query('SELECT * FROM reviews WHERE course_id = $1', [req.param('course')], function(err, result){
+					course.reviews = result.rows;
+					res.json(course);
+					done();
+				});
+			});
+		});
+  });
+};
--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,8 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/review', require('./api/review'));
+  app.use('/api/course', require('./api/course'));
   app.use('/api/courses', require('./api/courses'));
   app.use('/api/things', require('./api/thing'));
   
